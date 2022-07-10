bb74f3a659dcdbfbee463295c55401d220aaaa37
AlonaTR <alionatrubchaninova443@gmail.com>
2022-07-10 23:41:56 +0200

    Movie project

---
+++ django/movie_project/movie_project/movie_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+def show_all_movies(request):
+    movies =Movie.objects.all() 
+    return render(request, 'movie_app/all_movies.html', {'movies':movies})
+
+def show_one_movie(request, id_movie):
+    movie = get_object_or_404(Movie, id=id_movie)
+    return render(request, 'movie_app/one_movie.html', {'movie':movie})
